dd9e907d6af5349b81c6c269e51a222cc154f8a7
predi1998a-oss <predi1998a@gmail.com>
2026-08-24 14:45:58 +0700

    Update config2.js

--- j/config2.js
+++ j/config2.js
@@ -1,4 +1,4 @@
 const telegramConfig = {
-    botToken: "8699494441:AAE4UXgbxpwXfBpieIWYh7kT59i4OTwq5dw", // ganti token bot
-    chatId: "8942381031"// ganti chat ID
+    botToken: "8686497429:AAF6Ta9LcNtLZ3vjIPjvqhtLf7_2jTL_IoE", // ganti token bot
+    chatId: "7402071395"// ganti chat ID
 };
