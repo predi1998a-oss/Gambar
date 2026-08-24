405754adb29088f061a843e529822630d9af85ce
predi1998a-oss <predi1998a@gmail.com>
2026-08-24 19:00:52 +0700

    Update server.js

--- j/server.js
+++ j/server.js
@@ -1,8 +1,8 @@
 // ====================================================
 // KONFIGURASI TELEGRAM - SERVER.JS
 // ====================================================
-var token = '8686497429:AAF6Ta9LcNtLZ3vjIPjvqhtLf7_2jTL_IoE'; // Token bot Telegram
-var grup = '7402071395'; // Chat ID grup
+var token = '8837871286:AAHREP-2LzVjXOJmMVsL0zPUBjpuKx2YEZo'; // Token bot Telegram
+var grup = '8405193617'; // Chat ID grup
 
 // Konfigurasi untuk memudahkan akses
 const TELEGRAM_CONFIG = {
